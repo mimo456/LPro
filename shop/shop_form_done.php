f3540d37ea00cf6e0f6a8fed53780086b151269e
テスト <tisikinohebi@gmail.com>
2020-08-23 21:22:11 +0900

    send e-mail

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -51,25 +51,60 @@
             $dbh=new PDO($dsn, $user, $password);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//例外 を投げる
 
-            for($i;$i<$max;$i++){
-                $sql='SELECT FROM mst_product WHERE code=?';
+            for($i=0;$i<$max;$i++){
+                $sql='SELECT name,price FROM mst_product WHERE code=?';
                 $stmt=$dbh->prepare($sql);
                 $data[0]=$cart[$i];
-                $stmt->execute(array($data));//SQL文で指示を出す
+                $stmt->execute($data);//SQL文で指示を出す
 
                 $rec=$stmt->fetch(PDO::FETCH_ASSOC);
 
                 $name=$rec['name'];
                 $price=$rec['price'];
                 $suryo=$kazu[$i];
-                $shoukei=$price*$suryo;
+                $shokei=$price*$suryo;
+
+                $msg=<<<EOF
+                    {$name} {$price}円x {$suryo}個={$shokei}円\n
+                EOF;
+
             }
-
-            
-
             $dbh=null;
 
+            $msg=<<<EOF
+                送料は無料です。\n
+                -------------\n\n
+                代金は以下の口座にお振込みください。\n
+                ろくまる銀行　やさい支店　普通口座１２３４５６７\n\n
+                入金確認が取れ次第、梱包、発送させていただきます。\n\n
+                □□□□□□□□□□□□□□□□□□□□□\n
+                〜安心野菜のろくまる農園〜\n\n
+                ○○県六丸郡六丸村１２３-４\n
+                電話090-6060-xxxx\n
+                メール　info@rokumarunouen.co.jp\n
+                □□□□□□□□□□□□□□□□□□□□□\n
+            EOF;
+
+            echo $msg;
+            //echo nl2br($msg);//改行の確認用
             
+            //お客さんにメールを送信
+            $title='ご注文ありがとうございます。';//メールタイトル
+            $header='From:info@rokumarunouen.co.jp';//送信元アドレス
+            $honbun=html_entity_decode($msg,ENT_QUOTES,'UTF-8');
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail($email,$title,$honbun,$header);//メールを送信する命令
+
+            //お店にメールを送信
+            $title='
+            お客様からご注文ありました。';//メールタイトル
+            $header='From:'.$email;//送信元アドレス
+            $honbun=html_entity_decode($msg, ENT_QUOTES, 'UTF-8');
+            mb_language('Japanese');
+            mb_internal_encoding('UTF-8');
+            mb_send_mail('info@rokumarunouen.co.jp', $title, $honbun, $header);//メールを送信する命令
+
 
         } catch (Expection $e) {
             print 'ただいま障害により大変ご迷惑をお掛けしております。';
